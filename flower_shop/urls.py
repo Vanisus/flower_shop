f1cb0daf3b23b01a5ca0ec2897878b1c00fa2d3b
Vanisus <ieremin755@gmail.com>
2024-02-09 16:40:16 +0400

    Order section was added, order models and their styles were created

--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -7,6 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
+    path('orders/', include(('orders.urls', 'orders'), namespace='orders')),
     path('', include(('shop.urls', 'shop'), namespace='shop')),
 ]
 
